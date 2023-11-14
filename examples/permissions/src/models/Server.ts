dfb33759c690393774742616cb77ea641b91de87
João Silva <75593113+imjoaosilva@users.noreply.github.com>
2023-11-14 00:45:44 +0000

    ✨Adding BodyParser

--- examples/permissions/src/models/Server.ts
+++ examples/permissions/src/models/Server.ts
@@ -1,9 +1,10 @@
 import express, { Application } from 'express';
+import bodyParser from 'body-parser';
 
 export default class Server {
 
     // Creating a private variable of type Application (express.Application)
-    private app: express.Application;
+    private app: Application;
 
     // Creating a constructor to initialize the app variable
     constructor() {
@@ -17,6 +18,9 @@ export default class Server {
 
         // Creating a variable to store the port number (default: 3000)
         let PORT = process.env.PORT || 3000;
+
+        // Using body-parser to parse JSON bodies into JS objects
+        this.app.use(bodyParser.json());
 
         // Starting the server on the specified port
         this.app.listen(PORT, () => {
